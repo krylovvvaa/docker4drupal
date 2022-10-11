64aff6f081a5109256561af1d2607574864b8500
daria <krylova.dasha03@gmail.com>
2022-10-11 16:50:04 +0300

    services and block

---
+++ web/modules/custom/custom_features/src/Plugin/Block/UserBlock.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Drupal\custom_features\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a user block.
+ *
+ * @Block(
+ *   id = "custom_features_user",
+ *   admin_label = @Translation("User"),
+ *   category = @Translation("Custom")
+ * )
+ */
+class UserBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+  protected $entityTypeManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    $instance = new static($configuration, $plugin_id, $plugin_definition);
+    $instance->currentUser = $container->get('current_user');
+    $instance->entityTypeManager = $container->get('entity_type.manager');
+    return $instance;
+  }
+  /**
+   * Provides a 'Node Type' condition.
+   *
+   * @Condition(
+   *   id = "node_type",
+   *   label = @Translation("User Bundle"),
+   *   context_definitions = {
+   *     "node" = @ContextDefinition("entity:user", label = @Translation("User"))
+   *   }
+   * )
+   *
+   */
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    if(!$this->currentUser->isAuthenticated()) {
+      $build['content'] = [
+        '#markup' => $this->currentUser->getAccountName(),
+      ];
+      return $build;
+    }
+    $storage = $this->entityTypeManager->getStorage('user');
+    $user = $storage->load($this->currentUser->id());
+    $viewBuilder=$this->entityTypeManager->getViewBuilder('user');
+    $build['content'] = $viewBuilder->view($user, 'card');
+    return $build;
+  }
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheContexts() {
+    return['user.roles'];
+  }
+}
---
+++ web/modules/custom/custom_features/src/ProductCosts.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drupal\custom_features;
+/**
+ * Class RandomMessageGenerator
+ *
+ * @package Drupal\custom_features
+ */
+class ProductCosts
+{
+  private $costs;
+
+  public function getCosts($array) {
+    return $this->costs;
+  }
+}
